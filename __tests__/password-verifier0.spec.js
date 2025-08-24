4f34651c40c30d07fb2662893d7735cafd5b76dd
Andrey Babkin <ababkin@dataeast.com>
2025-08-24 12:09:26 +0700

    refactoring test for vp function

--- __tests__/password-verifier0.spec.js
+++ __tests__/password-verifier0.spec.js
@@ -1,11 +1,9 @@
 import { verifyPassword } from "../src/pwVerifier";
 
-describe("verifyPassword", () => {
-  test("given a failing rule, returns error", () => {
-    const fakeRule = (input) => ({ passed: false, reason: "fake reason" });
+test("verifyPassword, given a failing rule, returns error", () => {
+  const fakeRule = (input) => ({ passed: false, reason: "fake reason" });
 
-    const errors = verifyPassword("any value", [fakeRule]);
+  const errors = verifyPassword("any value", [fakeRule]);
 
-    expect(errors[0]).toContain("fake reason");
-  });
+  expect(errors[0]).toContain("fake reason");
 });
