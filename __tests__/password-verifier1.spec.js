868703d79f151869957d61f15796f9e63ccaa0ee
Andrey Babkin <ababkin@dataeast.com>
2025-08-24 12:10:10 +0700

    make pw class and test

---
+++ __tests__/password-verifier1.spec.js
@@ -0,0 +1,64 @@
+import { PasswordVerifier } from "../src/pwVerifierClass";
+
+const makeVerifier = () => new PasswordVerifier();
+const passingRule = (input) => ({ passed: true, reason: "" });
+
+const makeVerifierWithPassingRule = () => {
+  const verifier = makeVerifier();
+  verifier.addRule(passingRule);
+  return verifier;
+};
+
+const makeVerifierWithFailedRule = (reason) => {
+  const verifier = makeVerifier();
+  const fakeRule = (input) => ({ passed: false, reason: reason });
+  verifier.addRule(fakeRule);
+  return verifier;
+};
+
+describe("PasswordVerifier", () => {
+  describe("with a failing rule", () => {
+    test("has an error message based on the rule.reason", () => {
+      const verifier = makeVerifierWithFailedRule("fake reason");
+      const errors = verifier.verify("any input");
+
+      expect(errors[0]).toContain("fake reason");
+    });
+
+    test("has exactly one error", () => {
+      const verifier = makeVerifierWithFailedRule("fake reason");
+      const errors = verifier.verify("any input");
+
+      expect(errors.length).toBe(1);
+    });
+  });
+
+  describe("with a passing rule", () => {
+    test("has zero errors", () => {
+      const verifier = makeVerifierWithPassingRule();
+      const errors = verifier.verify("any input");
+
+      expect(errors.length).toBe(0);
+    });
+  });
+
+  describe("with a failing and passing rule", () => {
+    test("has one error", () => {
+      const verifier = makeVerifierWithFailedRule("fake reason");
+      verifier.addRule(passingRule);
+      const errors = verifier.verify("any input");
+
+      expect(errors.length).toBe(1);
+    });
+
+    test("error text belongs to failed rule", () => {
+      const verifier = makeVerifierWithFailedRule("fake reason");
+      verifier.addRule(passingRule);
+      const errors = verifier.verify("any input");
+
+      expect(errors[0]).toContain("fake reason");
+    });
+  });
+});
+
+// Using factory methods to avoid code duplication, encapsulating the state of each test
---
+++ src/pwVerifierClass.js
@@ -0,0 +1,23 @@
+export class PasswordVerifier {
+  constructor() {
+    this.rules = [];
+  }
+
+  addRule(rule) {
+    this.rules.push(rule);
+  }
+
+  verify(input) {
+    const errors = [];
+    
+    this.rules.forEach((rule) => {
+      const result = rule(input);
+
+      if (!result.passed) {
+        errors.push(`error ${result.reason}`);
+      }
+    });
+    
+    return errors;
+  }
+}
