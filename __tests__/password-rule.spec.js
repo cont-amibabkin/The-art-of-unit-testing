e8dbd25de93780f54025d505a5c7969a2f8235d6
Andrey Babkin <ababkin@dataeast.com>
2025-08-24 13:56:28 +0700

    create pwRule and parameterized test

---
+++ __tests__/password-rule.spec.js
@@ -0,0 +1,17 @@
+import { oneUpperCaseRule } from "../src/pwRule";
+
+describe("one uppercase rule", () => {
+  test("given no uppercase, it fails", () => {
+    const result = oneUpperCaseRule("abc");
+
+    expect(result.passed).toEqual(false);
+  });
+
+  test.each(["Abc", "aBc"])("given one uppercase, it passes", (input) => {
+    const result = oneUpperCaseRule(input);
+
+    expect(result.passed).toEqual(true);
+  });
+});
+
+// Parameterized test
