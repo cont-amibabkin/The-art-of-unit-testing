eb616772346a773c0a45f144a86ef87f8b0e3908
Andrey Babkin <ababkin@dataeast.com>
2025-08-31 17:43:14 +0700

    test for pw inject

--- __tests__/password-verifier-inject.spec.js
+++ __tests__/password-verifier-inject.spec.js
@@ -1,11 +1,26 @@
-import { verifyPasswordInject } from "../src/pwVerifierInject";
+import { verifyPasswordInject, inject } from "../src/pwVerifierInject";
 
 const SUNDAY = 0;
 const SATURDAY = 6;
-const MONDAY = 1;
+
+const injectDate = (newDay) => {
+  const reset = inject({
+    moment: function () {
+      return {
+        day: () => newDay,
+      };
+    },
+  });
+
+  return reset;
+};
 
 test("verifyPasswordInject, on weekends, throws exceptions", () => {
+  const reset = injectDate(SATURDAY);
+
   expect(() => {
-    verifyPasswordInject("anything", [], SUNDAY).toThrow("It\'s the weekend!")
-  })
-})
+    verifyPasswordInject("anything", [], SUNDAY).toThrow("It's the weekend!");
+  });
+
+  reset();
+});
